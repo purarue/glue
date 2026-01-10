17b37ddf166f8b66c2f26572eb5e39b9369e0333
purarue <7804791+purarue@users.noreply.github.com>
2026-01-09 16:37:16 -0800

    add curl to console

--- assets/frontend/react.js
+++ assets/frontend/react.js
@@ -6,6 +6,9 @@ import Computer from "./ui/computer";
 import { figlet } from "./figlet";
 console.log(figlet);
 console.log("Source Code: https://github.com/purarue/glue");
+console.log(
+  "You can fetch this in your terminal as well; run 'curl -L purarue.xyz'",
+);
 
 const container = document.getElementById("root");
 const root = createRoot(container);
