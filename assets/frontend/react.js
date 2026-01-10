54da91fe81705ef082a05f911bb3005668dd4787
purarue <7804791+purarue@users.noreply.github.com>
2026-01-09 16:43:22 -0800

    grammar

--- assets/frontend/react.js
+++ assets/frontend/react.js
@@ -7,7 +7,7 @@ import { figlet } from "./figlet";
 console.log(figlet);
 console.log("Source Code: https://github.com/purarue/glue");
 console.log(
-  "You can fetch this in your terminal as well; run 'curl -L purarue.xyz'",
+  "You can fetch this in your terminal as well! try 'curl -L purarue.xyz'",
 );
 
 const container = document.getElementById("root");
